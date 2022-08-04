44f9cf4a8751afe100663f183b63c0e3ce721839
Diego Cardoso <diegocar448@hotmail.com>
2022-08-04 16:06:09 -0300

    querybuilder

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -2,10 +2,9 @@ import {Request, Response} from 'express';
 import { CreateUserService } from '../services/CreateUserService';
 
 class CreateUserController {
-    async handle(request:Request, response: Response){
+    handle(request:Request, response: Response){
         const createUserService = new CreateUserService();
 
-        const id = request.body.id;
         const nome = request.body.nome;
         const email = request.body.email;
         
@@ -13,7 +12,7 @@ class CreateUserController {
         if(nome.length === 0 || email.length === 0){
             return response.status(400).json({mensagem: 'Preencha todos os campos'})
         }
-        const user = await createUserService.execute({id, nome, email})
+        const user = createUserService.execute({nome, email})
         return response.status(201).json({user})
     }
 }
