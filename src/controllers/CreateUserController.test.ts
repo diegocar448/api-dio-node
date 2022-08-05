05bd92d52375ce5dd933edb81e201fe6452049b1
Diego Cardoso <diegocar448@hotmail.com>
2022-08-05 10:24:51 -0300

    configurando o jest

---
+++ src/controllers/CreateUserController.test.ts
@@ -0,0 +1,15 @@
+describe('CreateUserController', () => {
+    function soma(a: number, b: number){
+        return a + b
+    }
+    const result = soma(1,2)
+
+    it('Deve retornar a soma de dois numeros', () => {
+        
+        expect(result).toBe(3)
+    })
+
+    it('Não deve retornar um resultado diferente de 3', () => {
+        expect(result).not.toBe(5)
+    })
+})
